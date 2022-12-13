e29b46eb37e18b30a85a9e55add2881eed149834
sarwjeet424 <sarwjeet424@gmail.com>
2022-12-13 12:38:53 +0530

    changes

--- controller/urlController.js
+++ controller/urlController.js
@@ -46,7 +46,7 @@ const getUrl= async function (req,res){
         let urlCode=req.params.urlCode
         if(!shortid.isValid(urlCode)) return res.status(500).send({status:false,message:"invalid urlCode"})
         let data= await urlModel.findOne({urlCode})
-        if(!data) return res.status(400).send({status:false,message:"invalid urlCode"})
+        if(!data) return res.status(404).send({status:false,message:"urlCode not found"})
         return res.status(302).redirect(data.longUrl)
 
     }catch(error){
