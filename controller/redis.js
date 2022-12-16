2467d1d246916286acda1ddd1d18f7465bdce157
vikasgautam1234 <vikasmtr1234@gmail.com>
2022-12-16 12:53:04 +0530

    done

--- controller/redis.js
+++ controller/redis.js
@@ -4,23 +4,23 @@ const { promisify } = require("util");
 
 //1. Connect to the redis server
 const redisClient = redis.createClient(
-  16229,
-  "redis-16229.c212.ap-south-1-1.ec2.cloud.redislabs.com",
+  17112,
+  "redis-17112.c264.ap-south-1-1.ec2.cloud.redislabs.com",
   { no_ready_check: true }
 );
-redisClient.auth("znaLjzm6rykCQV9nqY3cqire54HyQqdB", function (err) {
+redisClient.auth("xest3pC118Sn1P6qAjXUadLzqELXLZzk", function (err) {
   if (err) throw err;
 });
 
-redisClient.on("connect", async function () {
+redisClient.on("connect", async function () {       // "connect" is fixed nothing can be accepted other than connect
   console.log("Connected to Redis..");
 });
 
 
 //2. Prepare the functions for each command
 
-const SET_ASYNC = promisify(redisClient.SETEX).bind(redisClient);
-const GET_ASYNC = promisify(redisClient.GET).bind(redisClient);
+const SET_ASYNC = promisify(redisClient.SETEX).bind(redisClient);    // bind() is the function of javascript
+const GET_ASYNC = promisify(redisClient.GET).bind(redisClient);      
 
 
 module.exports={SET_ASYNC,GET_ASYNC}
